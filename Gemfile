409be1825327cfe146c5de6dbf816e50b124dacf
Patric Pereira <patric.pereirafranca@gmail.com>
2019-02-25 19:43:15 -0300

    initial commit and configuration

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+
+ruby '2.4.1'
+
+gem 'capybara'
+gem 'chromedriver-helper'
+gem 'cucumber'
+gem 'rspec'
+gem 'selenium-webdriver'
+gem 'bundler'
---
+++ features/support/env.rb
@@ -0,0 +1,10 @@
+require 'capybara'
+require 'capybara/cucumber'
+require 'selenium-webdriver'
+
+Capybara.configure do |config|
+  config.default_driver = :selenium_chrome
+  config.app_host = 'https://verity.com.br'
+end
+
+Capybara.default_max_wait_time = 5
